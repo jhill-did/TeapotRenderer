6bff500df4d08908fbdffdbf820baec9d364b4c3
Jordan Hill <puddin482@yahoo.com>
2017-08-27 23:54:44 -0500

    Added vertex normal generation and interpolation.

--- Core.js
+++ Core.js
@@ -213,11 +213,10 @@ class Canvas {
 
   getDepth(screenPos) {
     const index = this.canvasWidth * (screenPos.y + (this.canvasHeight / 2)) + screenPos.x + (this.canvasWidth / 2);
-    debugger;
     return this.zBuffer[index];
   }
 
-  calculateFragment(face, color) {
+  calculateFragment(face, normals, color) {
     let topLeft = {
       x: Math.min(face.v1.x, face.v2.x, face.v3.x),
       y: Math.min(face.v1.y, face.v2.y, face.v3.y)
@@ -265,9 +264,42 @@ class Canvas {
           // let testColor = new Vector4(interpolatedDepth, interpolatedDepth, interpolatedDepth, 255);
           let interpolatedDepth = (1 / (barycentric.x * (1 / face.v1.z) + barycentric.y * (1 / face.v2.z) + barycentric.z * (1 / face.v3.z)));
           let currentDepth = this.getDepth({x, y});
-          debugger;
           if (interpolatedDepth >= -1 && interpolatedDepth <= 1 && interpolatedDepth < currentDepth) {
-            this.setPixel({x, y}, color, interpolatedDepth);
+            // let interpolatedNorm1 = {
+            //   x: barycentric.x * (1 / normals.n1.x),
+            //   y: barycentric.x * (1 / normals.n1.y),
+            //   z: barycentric.x * (1 / normals.n1.z)
+            // };
+
+            // let interpolatedNorm2 = {
+            //   x: barycentric.y * (1 / normals.n2.x),
+            //   y: barycentric.y * (1 / normals.n2.y),
+            //   z: barycentric.y * (1 / normals.n2.z)
+            // };
+
+            // let interpolatedNorm3 = {
+            //   x: barycentric.z * (1 / normals.n3.x),
+            //   y: barycentric.z * (1 / normals.n3.y),
+            //   z: barycentric.z * (1 / normals.n3.z)
+            // };
+
+            // let interpolatedNormal = {
+            //   x: 1 / (interpolatedNorm1.x + interpolatedNorm2.x + interpolatedNorm3.x),
+            //   y: 1 / (interpolatedNorm1.y + interpolatedNorm2.y + interpolatedNorm3.y),
+            //   z: 1 / (interpolatedNorm1.z + interpolatedNorm2.z + interpolatedNorm3.z)
+            // };
+
+            const interpolatedNormal =
+              normals.n1.scale(barycentric.x).add(
+              normals.n2.scale(barycentric.y)).add(
+              normals.n3.scale(barycentric.z));
+            debugger;
+
+            const red = Math.floor((interpolatedNormal.x + 1) / 2 * 255);
+            const green = Math.floor((interpolatedNormal.y + 1) / 2 * 255);
+            const blue = Math.floor((interpolatedNormal.z + 1) / 2 * 255);
+            const finalColor = new Vector4(red, green, blue, 255);
+            this.setPixel({x, y}, finalColor, interpolatedDepth);
           }
         }
       }
